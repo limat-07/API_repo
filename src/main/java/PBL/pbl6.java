e63030c3df27fe1e307d258a8b90c28a4ead286e
limat-07 <whistler.20463982@gmail.com>
2023-05-18 14:01:54 +0530

    commit

---
+++ src/main/java/PBL/pbl6.java
@@ -0,0 +1,16 @@
+package PBL;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class pbl6 {
+	@Test
+	public void test_deleteUser() {
+		RestAssured.useRelaxedHTTPSValidation();
+
+		RestAssured.baseURI = "https://reqres.in";
+
+		RestAssured.when().delete("/api/users/2").then().statusCode(204).log().all();
+	}
+}
